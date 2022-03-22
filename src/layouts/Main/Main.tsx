af2edb33d177184936b32e640d2688d562663c00
Sérgio Félix <sergio98@outlook.pt>
2022-03-22 20:37:12 +0000

    Main app layout

---
+++ src/layouts/Main/Main.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+// Components
+import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
+import Scroll from '../../components/Scroll/Scroll';
+
+const Main: React.FunctionComponent = () => {
+	return (
+		<div>
+			<header>
+				<Navbar />
+			</header>
+			<main>
+				<Outlet />
+				<Scroll />
+			</main>
+			<footer>
+				<Footer />
+			</footer>
+		</div>
+	);
+};
+
+export default Main;
