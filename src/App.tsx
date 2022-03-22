fe0fc78bb8610e6c91b84124381bffbb11fbe455
Sérgio Félix <sergio98@outlook.pt>
2022-03-22 20:35:44 +0000

    Router system with React-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Layout
+import Layout from './layouts/Main/Main';
+
+// Pages
+import Homepage from './pages/Homepage';
+
+const App: React.FunctionComponent = () => {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Homepage />} />
+				</Route>
+			</Routes>
+		</Router>
+	);
+};
 
 export default App;
