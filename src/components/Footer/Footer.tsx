52b7d5e7f3b396f4e600a303836f98ff1dc02e3e
Sérgio Félix <sergio98@outlook.pt>
2022-03-22 20:36:13 +0000

    Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer: React.FunctionComponent = () => {
+	return (
+        <div>
+            <h1>Footer</h1>
+        </div>
+    );
+};
+
+export default Footer;
