2fac6f571be58129b7cb6301e23c16e768d16602
Sérgio Félix <sergio98@outlook.pt>
2022-03-22 20:36:58 +0000

    Scroll widget component

---
+++ src/components/Scroll/Scroll.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import { ChevronUpIcon } from '@heroicons/react/solid';
+
+const Scroll: React.FunctionComponent = () => {
+	const [isVisible, setIsVisible] = React.useState<Boolean>(false);
+
+	const toggleVisibility = () => {
+		if (window.pageYOffset > 300) {
+			setIsVisible(true);
+		} else {
+			setIsVisible(false);
+		}
+	};
+
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
+
+	React.useEffect(() => {
+		window.addEventListener('scroll', toggleVisibility);
+		return () => {
+			window.removeEventListener('scroll', toggleVisibility);
+		};
+	}, []);
+	return (
+		<div className="fixed bottom-2 right-2">
+			<button type="button" onClick={scrollToTop} className={`p-3 rounded-full shadow-sm text-white bg-blue-600 ease-linear duration-500 animate-bounce ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+				<ChevronUpIcon className="w-6 h-6" />
+			</button>
+		</div>
+	);
+};
+
+export default Scroll;
