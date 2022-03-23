3ac00044766640bb2169f0d6fe1fc86471c79cbe
Sérgio Félix <sergio98@outlook.pt>
2022-03-23 18:08:10 +0000

    navbar finished, -refactoring

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Popover, Transition } from '@headlessui/react';
-import { ChartBarIcon, CursorClickIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
+import { ChartBarIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 import { ChevronDownIcon } from '@heroicons/react/solid';
 import logo from '../../assets/logo.gif';
-
-const navlinks = [
-    {
-        name: 'Analytics',
-        description: 'Get a better understanding of where your traffic is coming from.',
-        href: '#',
-        icon: ChartBarIcon,
-    },
-    {
-        name: 'Engagement',
-        description: 'Speak directly to your customers in a more meanigful way.',
-        href: '#',
-        icon: CursorClickIcon,
-    },
-]
 
 const Navbar: React.FunctionComponent = () => {
 	return (
@@ -31,85 +16,51 @@ const Navbar: React.FunctionComponent = () => {
 					</Link>
 				</div>
 				<div className="md:hidden">
-					<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary">
+					<Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-600 hover:bg-gray-100 hover:text-gray-500 focus:outline-none">
 						<span className="sr-only">Open menu</span>
 						<MenuIcon className="h-8 w-8" aria-hidden="true" />
 					</Popover.Button>
 				</div>
 				<Popover.Group as="nav" className="hidden space-x-10 md:flex">
+					<NavLink to="/" className={({ isActive }) => (isActive ? 'text-base font-medium text-gray-600 hover:text-gray-500 border-b border-b-gray-900' : 'text-base font-medium text-gray-600 hover:text-gray-500')}>
+						Home
+					</NavLink>
 					<Popover className="relative">
 						{({ open }) => (
 							<>
-								<Popover.Button className={`${open ? 'text-gray-900' : 'text-gray-500'} group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}>
-									<span>Solutions</span>
-									<ChevronDownIcon className={`${open ? 'text-gray-600' : 'text-gray-400'} ml-2 h-5 w-5 group-hover:text-gray-500`} aria-hidden="true" />
-								</Popover.Button>
-								<Transition as={React.Fragment} enter="transition ease-out duration-200" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100 translate-y-0" leave="transition ease-in duration-150" leaveFrom="opacity-100 translate-y-0" leaveTo="opacity-0 translate-y-1">
-									<Popover.Panel className="absolute z-10 -ml-4 mt-3 w-screen max-w-md transform lg:left-1/2 lg:ml-0 lg:max-w-2xl lg:-translate-x-1/2">
-										<div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-											<div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8 lg:grid-cols-2">
-												<a href="##" className="-m-3 flex items-start rounded-lg p-3 hover:bg-gray-50">
-													<div className="flex h-10 w-10 flex-shrink-0 items-center justify-center rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-														<ChartBarIcon className="h-6 w-6" aria-hidden="true" />
-													</div>
-													<div className="ml-4">
-														<p className="text-base font-medium text-gray-900">
-                                                            TEST
-                                                        </p>
-														<p className="mt-1 text-sm text-gray-500">
-                                                            TEST TEST TEST
-                                                        </p>
-													</div>
-												</a>
-											</div>
-											<div className="bg-gray-50 p-5 sm:p-8">
-												<a href="##" className="-m-3 flow-root rounded-md p-3 hover:bg-gray-100">
-													<div className="flex items-center">
-														<div className="text-base font-medium text-gray-900">
-															Enterprise
-														</div>
-														<span className="ml-3 inline-flex items-center rounded-full bg-indigo-100 px-3 py-0.5 text-xs font-medium leading-5 text-indigo-800">
-															New
-														</span>
-													</div>
-													<p className="mt-1 text-sm text-gray-500">
-														Empower your entire team
-														with even more advanced
-														tools.
-													</p>
-												</a>
-											</div>
-										</div>
-									</Popover.Panel>
-								</Transition>
-							</>
-						)}
-					</Popover>
-					<a href="##" className="text-base font-medium text-gray-500 hover:text-gray-900">
-						Pricing
-					</a>
-					<a href="##" className="text-base font-medium text-gray-500 hover:text-gray-900">
-						Docs
-					</a>
-					<Popover className="relative">
-						{({ open }) => (
-							<>
-								<Popover.Button className={`${open ? 'text-gray-900' : 'text-gray-500'} group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2`}>
-									<span>More</span>
-									<ChevronDownIcon className={`${open ? 'text-gray-600' : 'text-gray-400'} ml-2 h-5 w-5 group-hover:text-gray-500`} aria-hidden="true" />
+								<Popover.Button className={`${open ? 'text-gray-500' : 'text-gray-600'} group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-500 focus:outline-none`}>
+									<span>Soluções</span>
+									<ChevronDownIcon className={`${open ? 'text-gray-500' : 'text-gray-600'} ml-2 h-5 w-5 group-hover:text-gray-500`} aria-hidden="true" />
 								</Popover.Button>
 								<Transition as={React.Fragment} enter="transition ease-out duration-200" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100 translate-y-0" leave="transition ease-in duration-150" leaveFrom="opacity-100 translate-y-0" leaveTo="opacity-0 translate-y-1">
 									<Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
-										<div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
-											<div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-												<a href="##" className="-m-3 block rounded-md p-3 hover:bg-gray-50">
-													<p className="text-base font-medium text-gray-900">
-														Test
+										<div className="overflow-hidden shadow-lg ring-1 ring-black ring-opacity-5">
+											<div className="relative bg-white">
+												<NavLink to="solucoes/comercio-digital" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Comércio Digital
 													</p>
-													<p className="mt-1 text-sm text-gray-500">
-														Test Test Test
+												</NavLink>
+												<NavLink to="solucoes/marketing-digital-performance" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Marketing Digital e Performance
 													</p>
-												</a>
+												</NavLink>
+												<NavLink to="solucoes/uiux-design" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														UI/UI Design
+													</p>
+												</NavLink>
+												<NavLink to="solucoes/websites-workplaces-digitais" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Websites e Workplaces Digitais
+													</p>
+												</NavLink>
+												<NavLink to="solucoes/web-development" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Web Development
+													</p>
+												</NavLink>
 											</div>
 										</div>
 									</Popover.Panel>
@@ -117,13 +68,54 @@ const Navbar: React.FunctionComponent = () => {
 							</>
 						)}
 					</Popover>
+					<NavLink to="sobre-a-basicamente" className={({ isActive }) => (isActive ? 'text-base font-medium text-gray-500 hover:text-gray-600 border-b border-b-gray-900' : 'text-base font-medium text-gray-500 hover:text-gray-600')}>
+						Sobre
+					</NavLink>
+					<Popover className="relative">
+						{({ open }) => (
+							<>
+								<Popover.Button className={`${open ? 'text-gray-500' : 'text-gray-600'} group inline-flex items-center rounded-md bg-white text-base font-medium hover:text-gray-500 focus:outline-none`}>
+									<span>Work</span>
+									<ChevronDownIcon className={`${open ? 'text-gray-500' : 'text-gray-600'} ml-2 h-5 w-5 group-hover:text-gray-500`} aria-hidden="true" />
+								</Popover.Button>
+								<Transition as={React.Fragment} enter="transition ease-out duration-200" enterFrom="opacity-0 translate-y-1" enterTo="opacity-100 translate-y-0" leave="transition ease-in duration-150" leaveFrom="opacity-100 translate-y-0" leaveTo="opacity-0 translate-y-1">
+									<Popover.Panel className="absolute left-1/2 z-10 mt-3 w-screen max-w-xs -translate-x-1/2 transform px-2 sm:px-0">
+										<div className="overflow-hidden shadow-lg ring-1 ring-black ring-opacity-5">
+											<div className="relative bg-white">
+												<NavLink to="work/websites" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Websites
+													</p>
+												</NavLink>
+												<NavLink to="work/e-commerce" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														eCommerce
+													</p>
+												</NavLink>
+												<NavLink to="work/web-development" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Web Development
+													</p>
+												</NavLink>
+												<NavLink to="work/marketing-digital-performance" className="block px-8 py-2 hover:bg-gray-100">
+													<p className="text-base font-medium text-gray-600">
+														Marketing Digital e Performance
+													</p>
+												</NavLink>
+											</div>
+										</div>
+									</Popover.Panel>
+								</Transition>
+							</>
+						)}
+					</Popover>
+					<NavLink to="contactos" className={({ isActive }) => (isActive ? 'text-base font-medium text-gray-500 hover:text-gray-600 border-b border-b-gray-900' : 'text-base font-medium text-gray-500 hover:text-gray-600')}>
+						Contactos
+					</NavLink>
 				</Popover.Group>
 				<div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-					<a href="##" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">
-						Sign In
-					</a>
-					<a href="##" className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">
-						Sign Up
+					<a href="##" className="inline-flex items-center justify-center whitespace-nowrap rounded-none border border-transparent bg-primary px-8 py-3.5 text-xs font-medium text-white shadow-sm hover:opacity-80">
+						Pedir Proposta
 					</a>
 				</div>
 			</div>
