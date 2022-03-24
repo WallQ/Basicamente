1ca5bd9cc0db328a9b06bd0126f4dde33df36de0
Sérgio Félix <sergio98@outlook.pt>
2022-03-24 21:31:56 +0000

    More compatible globally

--- src/components/SimpleCard/SimpleCard.tsx
+++ src/components/SimpleCard/SimpleCard.tsx
@@ -2,41 +2,56 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 interface Props {
+	size?: string;
 	image: string;
 	altText: string;
 	title: string;
-	subTitle: string;
+	subTitle?: string;
 	text: string;
-	urlTitle: string;
-	href: string;
+	urlTitle?: string;
+	internalHref?: string;
+	externalHref?: string;
 }
 
 const SimpleCard: React.FunctionComponent<Props> = ({
+	size,
 	image,
 	altText,
 	title,
 	subTitle,
 	text,
 	urlTitle,
-	href,
+	internalHref,
+	externalHref,
 }) => {
 	return (
-        <>
-            <div className='flex flex-col whitespace-pre-wrap max-w-xs'>
+		<>
+			<div className={`flex ${size} flex-col whitespace-pre-wrap`}>
 				<img src={image} alt={altText} />
-				<p className='text-sm font-bold text-gray-600 mt-8'>
-					{title}
-				</p>
-				<p className='text-sm font-light text-gray-600 mt-4'>
-					{subTitle}
-				</p>
-				<p className='text-sm font-light text-gray-600 mt-4'>
-					{text}
-				</p>
-				<Link className='mt-4' to={href}><span className='font-thin text-sm text-primary border-b border-b-primary' >{urlTitle}</span></Link>
+				<p className="mt-8 text-sm font-bold text-gray-600">{title}</p>
+				{subTitle && (
+					<p className="mt-4 text-sm font-light text-gray-600">
+						{subTitle}
+					</p>
+				)}
+				<p className="mt-4 text-sm font-light text-gray-600">{text}</p>
+				{internalHref && (
+					<Link className="mt-4" to={internalHref}>
+						<span className="border-b border-b-primary text-sm font-thin text-primary">
+							{urlTitle}
+						</span>
+					</Link>
+				)}
+				{externalHref && (
+					<a className="mt-4" href={externalHref}>
+						<span className="border-b border-b-primary text-sm font-thin text-primary">
+							{urlTitle}
+						</span>
+					</a>
+				)}
 			</div>
-        </>
-    );
+		</>
+	);
 };
 
 export default SimpleCard;
