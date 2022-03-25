28db5fdedc2b3017fbbde0df44ba627e58e57729
Sérgio Félix <sergio98@outlook.pt>
2022-03-25 17:07:29 +0000

    New sections: Partners, Contact

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -4,29 +4,41 @@ import React from 'react';
 import BreakTitle from '../components/BreakTitle/BreakTitle';
 import SimpleCard from '../components/SimpleCard/SimpleCard';
 
-// Images
+// Background Images
 import BannerImage from '../assets/Homepage/Basicamente-Header.jpeg';
+import FooterImage from '../assets/Homepage/Basicamente-Footer.jpg';
+
+// Card Images
 import ComércioDigitalImage from '../assets/Homepage/eCommerce.png';
 import MarketingDigitalPerformanceImage from '../assets/Homepage/MarketingPerformance.png';
 import UIUXDesignImage from '../assets/Homepage/UIUxDesign.png';
 import WebDevelopmentImage from '../assets/Homepage/Software,CRM,Data.png';
 
+// Works Images
 import eCommerceMahrla from '../assets/Homepage/eCommerce-Mahrla.png';
 import eCommerceB2Jovaty from '../assets/Homepage/eCommerceB2Jovaty.png';
 import CaseParissete from '../assets/Homepage/Case-Parissete.png';
+
+// Partner Images
+import Partner1 from '../assets/Homepage/Partner-1.png';
+import Partner2 from '../assets/Homepage/Partner-2.png';
+import Partner3 from '../assets/Homepage/Partner-3.png';
+import Partner4 from '../assets/Homepage/Partner-4.png';
+import Partner5 from '../assets/Homepage/Partner-5.png';
+import Partner6 from '../assets/Homepage/Partner-6.png';
 
 const Homepage: React.FunctionComponent = () => {
 	return (
 		<>
 			<div
-				className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-44"
+				className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-28 lg:py-44"
 				style={{ backgroundImage: `url(${BannerImage})` }}
 			>
-				<div className="m-auto w-1/2 space-y-8 text-center leading-loose text-white">
-					<h1 className="text-4xl font-medium">
+				<div className="m-auto w-2/3 space-y-8 text-center leading-loose text-white lg:w-1/2">
+					<h1 className="text-xl font-medium lg:text-4xl">
 						Aceleramos a sua Transformação Digital
 					</h1>
-					<p className="text-2xl font-light leading-normal">
+					<p className="text-base font-light leading-normal lg:text-2xl">
 						Ajudamos empresas e organizações a{' '}
 						<span className="font-bold">
 							Transformar, Inovar e a Escalar Negócios no Digital
@@ -41,11 +53,11 @@ const Homepage: React.FunctionComponent = () => {
 					</p>
 				</div>
 			</div>
-			<div className="mx-auto max-w-max px-4">
+			<div className="mx-auto max-w-max px-8">
 				<BreakTitle title="Em que podemos ajudar?" />
-				<div className="flex flex-col flex-wrap items-center md:items-start justify-evenly gap-x-10 gap-y-10 align-middle md:flex-row">
+				<div className="flex min-w-full flex-col flex-wrap items-center justify-evenly gap-x-8 gap-y-10 align-middle md:flex-row md:items-start">
 					<SimpleCard
-						size='max-w-xs'
+						size="max-w-xs"
 						image={ComércioDigitalImage}
 						altText="Comércio Digital"
 						title="Comércio Digital"
@@ -55,7 +67,7 @@ const Homepage: React.FunctionComponent = () => {
 						internalHref="solucoes/comercio-digital"
 					/>
 					<SimpleCard
-						size='max-w-xs'
+						size="max-w-xs"
 						image={MarketingDigitalPerformanceImage}
 						altText="Marketing Digital e Performance"
 						title="Marketing Digital e Performance"
@@ -67,7 +79,7 @@ const Homepage: React.FunctionComponent = () => {
 						internalHref="solucoes/marketing-digital-performance"
 					/>
 					<SimpleCard
-						size='max-w-xs'
+						size="max-w-xs"
 						image={UIUXDesignImage}
 						altText="UI/UX Design"
 						title="UI/UX Design"
@@ -79,7 +91,7 @@ const Homepage: React.FunctionComponent = () => {
 						internalHref="solucoes/uiux-design"
 					/>
 					<SimpleCard
-						size='max-w-xs'
+						size="max-w-xs"
 						image={WebDevelopmentImage}
 						altText="Web Development"
 						title="Web Development"
@@ -92,9 +104,9 @@ const Homepage: React.FunctionComponent = () => {
 					/>
 				</div>
 				<BreakTitle title="Projetos em destaque" />
-				<div className="flex flex-col flex-wrap items-center md:items-start justify-evenly gap-x-8 gap-y-10 align-middle md:flex-row">
+				<div className="flex flex-col flex-wrap items-center justify-evenly gap-x-8 gap-y-10 align-middle md:flex-row md:items-start">
 					<SimpleCard
-						size='max-w-md'
+						size="max-w-md"
 						image={eCommerceMahrla}
 						altText="eCommerce B2C Mahrla"
 						title="eCommerce B2C Mahrla"
@@ -103,7 +115,7 @@ const Homepage: React.FunctionComponent = () => {
 						externalHref="mailto:askus@basicamente.io?subject=Quero%20saber%20mais%20sobre%20o%20projeto%20Mahrla"
 					/>
 					<SimpleCard
-						size='max-w-md'
+						size="max-w-md"
 						image={eCommerceB2Jovaty}
 						altText="eCommerce B2B Jovaty"
 						title="eCommerce B2B Jovaty"
@@ -112,7 +124,7 @@ const Homepage: React.FunctionComponent = () => {
 						externalHref="mailto:askus@basicamente.io?subject=Quero%20saber%20mais%20sobre%20o%20projeto%20Mahrla"
 					/>
 					<SimpleCard
-						size='max-w-md'
+						size="max-w-md"
 						image={CaseParissete}
 						altText="Data Collector Paris:sete"
 						title="Data Collector Paris:sete"
@@ -121,6 +133,146 @@ const Homepage: React.FunctionComponent = () => {
 						externalHref="mailto:askus@basicamente.io?subject=Quero%20saber%20mais%20sobre%20o%20projeto%20Mahrla"
 					/>
 				</div>
+				<BreakTitle title="Dezenas de marcas confiam na Basicamente " />
+				<div className="grid grid-cols-3 content-center justify-items-center gap-y-6 pb-12">
+					<img 
+						className="w-4/5" 
+						src={Partner1} 
+						alt="Porto. & hôma" 
+					/>
+					<img
+						className="w-4/5"
+						src={Partner2}
+						alt="mota-engil & Forcargo"
+					/>
+					<img
+						className="w-4/5"
+						src={Partner3}
+						alt="P.Porto & Kaizen"
+					/>
+					<img
+						className="w-4/5"
+						src={Partner4}
+						alt="321 Crédito & Halibut"
+					/>
+					<img
+						className="w-4/5"
+						src={Partner5}
+						alt="Mattreness & Clínicas Nuno Mendes"
+					/>
+					<img
+						className="w-4/5"
+						src={Partner6}
+						alt="Exaktus & Mahrla"
+					/>
+				</div>
+			</div>
+			<div className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-16 lg:py-28" style={{ backgroundImage: `url(${FooterImage})` }}>
+				<div className="space-y-8 text-center leading-loose text-white lg:w-1/2">
+					<h1 className="text-xl font-medium lg:text-3xl">
+						Basicamente, vamos conversar?
+					</h1>
+					<p className="text-base font-light leading-normal">
+						Teremos ao seu dispor uma equipa de profissionais
+						disponível a ajudá-lo.
+					</p>
+				</div>
+				<form className='flex flex-col flex-wrap text-white mt-8 w-1/2 justify-center gap-y-6'>
+					<div className='inline-flex gap-x-2 w-full justify-between'>
+						<div className='flex flex-col grow'>
+							<label htmlFor="name" className="block text-sm font-thin text-white">
+                                Nome *
+                            </label>
+                            <div className="mt-1">
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className="block w-full rounded-sm border-gray-300 py-2.5 pl-4 text-gray-600 shadow-sm focus:ring-4 focus:ring-white sm:text-sm"
+                                    aria-describedby="name-description"
+                                />
+                            </div>
+                            <p className="mt-2 text-sm font-thin text-white" id="name-description">
+                                Nome
+                            </p>
+						</div>
+						<div className='flex flex-col grow'>
+                            <div className="mt-6">
+                                <input
+                                    type="text"
+                                    name="surname"
+                                    id="surname"
+                                    className="block w-full rounded-sm border-gray-300 py-2.5 pl-4 text-gray-600 shadow-sm focus:ring-4 focus:ring-white sm:text-sm"
+                                    aria-describedby="surname-description"
+                                />
+                            </div>
+                            <p className="mt-2 text-sm font-thin text-white" id="surname-description">
+                                Sobrenome
+                            </p>
+						</div>
+					</div>
+					<div className='flex flex-col'>
+						<label htmlFor="email" className="block text-sm font-thin text-white">
+							O seu e-mail *
+						</label>
+						<div className="mt-1">
+							<input
+								type="email"
+								name="email"
+								id="email"
+								className="block w-full rounded-sm border-gray-300 py-2.5 pl-4 text-gray-600 shadow-sm focus:ring-4 focus:ring-white sm:text-sm"
+								aria-describedby="email-description"
+							/>
+						</div>
+					</div>
+					<div className='flex flex-col'>
+						<label htmlFor="telephone" className="block text-sm font-thin text-white">
+							Telefone *
+						</label>
+						<div className="mt-1">
+							<input
+								type="text"
+								name="telephone"
+								id="telephone"
+								className="block w-full rounded-sm border-gray-300 py-2.5 pl-4 text-gray-600 shadow-sm focus:ring-4 focus:ring-white sm:text-sm"
+								aria-describedby="telephone-description"
+							/>
+						</div>
+					</div>
+					<div className='flex flex-col'>
+						<label htmlFor="company" className="block text-sm font-thin text-white">
+							A sua empresa *
+						</label>
+						<div className="mt-1">
+							<input
+								type="text"
+								name="company"
+								id="company"
+								className="block w-full rounded-sm border-gray-300 py-2.5 pl-4 text-gray-600 shadow-sm focus:ring-4 focus:ring-white sm:text-sm"
+								aria-describedby="company-description"
+							/>
+						</div>
+					</div>
+					<div className='flex flex-col'>
+						<label htmlFor="message" className="block text-sm font-thin text-white">
+							Conte-nos o seu projeto *
+						</label>
+						<div className="mt-1">
+							<textarea
+                                rows={3}
+                                name="message"
+                                id="message"
+                                className="block w-full rounded-sm border-gray-300 py-2.5 pl-4 text-gray-600 shadow-sm focus:ring-4 focus:ring-white sm:text-sm"
+                                aria-describedby="message-description"
+                            />
+						</div>
+					</div>
+					<div className='flex flex-row justify-center'>
+						<button type="submit" className="whitespace-nowrap rounded-none border border-transparent bg-primary px-8 py-4 text-sm font-thin text-white shadow-sm hover:opacity-80">
+							Desejo ser contactado
+						</button>
+					</div>
+				</form>
 			</div>
 		</>
 	);
