a6e18adf7ee796d5bfa81394d1b350a2076eb778
Sérgio Félix <sergio98@outlook.pt>
2022-03-25 17:06:50 +0000

    Navlinks external file

---
+++ src/components/Navbar/_navItems.tsx
@@ -0,0 +1,41 @@
+export const solucoes = [
+	{
+		routeTitle: 'Comércio Digital',
+		routeUrl: 'solucoes/comercio-digital',
+	},
+	{
+		routeTitle: 'Marketing Digital e Performance',
+		routeUrl: 'solucoes/marketing-digital-performance',
+	},
+	{
+		routeTitle: 'UI/UI Design',
+		routeUrl: 'solucoes/uiux-design',
+	},
+	{
+		routeTitle: 'Websites e Workplaces Digitais',
+		routeUrl: 'solucoes/websites-workplaces-digitais',
+	},
+	{
+		routeTitle: 'Web Development',
+		routeUrl: 'solucoes/web-development',
+	},
+];
+
+export const work = [
+	{
+		routeTitle: 'Websites',
+		routeUrl: 'work/websites',
+	},
+	{
+		routeTitle: 'eCommerce',
+		routeUrl: 'work/e-commerce',
+	},
+	{
+		routeTitle: 'Web Development',
+		routeUrl: 'work/web-development',
+	},
+	{
+		routeTitle: 'Marketing Digital e Performance',
+		routeUrl: 'work/marketing-digital-performance',
+	},
+];
