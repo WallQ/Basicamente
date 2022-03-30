1c4fb88177de16dfb6b10fb7277c64b53cff65be
Sérgio Félix <sergio98@outlook.pt>
2022-03-30 20:35:42 +0100

    Data: partners for Sobre

---
+++ src/pages/Sobre/_data.tsx
@@ -0,0 +1,74 @@
+export const partners = [
+	{
+		image: require('../../assets/Sobre/Partner-1.png'),
+		altText: 'Porto. & hôma & 321 Crédito',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-2.png'),
+		altText: 'Halibut. & mota-engil & Forcargo',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-3.png'),
+		altText: 'P.Porto & Clínicas Nuno Mendes & Kaizen',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-4.png'),
+		altText: 'Bibropedra & Peixoto & Peixoto & Parque Aquático Amarante',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-5.png'),
+		altText: 'Telbac & fielnorte & M.Cunha',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-6.png'),
+		altText: 'Avessada Enoteca & FT System & FCC Arquitectura',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-7.png'),
+		altText: 'Campus Clinic & CRS Manufactures & Leal Pinheiro',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-8.png'),
+		altText: 'Exaktus & J. Amorim & Quelhas Dias & Edilages',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-9.png'),
+		altText: 'tPS & Luc & Me Allegro',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-10.png'),
+		altText: 'Jeff & Joalharia Naná & Amadeu',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-11.png'),
+		altText: 'Elmee & Essential Store & Fradizela',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-12.png'),
+		altText: 'Jovaty & ARC & Mattreness',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-13.png'),
+		altText: 'papelinf & different & biokimica',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-14.png'),
+		altText: 'Carviai & Fitbike & Mahrla',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-15.png'),
+		altText: 'Hotel Havarras & Amarante & RBT Construção',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-16.png'),
+		altText: 'O Sítio Dos Cogumelos & Paris:Sete & PEMI',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-17.png'),
+		altText: 'Estrada Real & Extrenato Senhora do Carmo & Irmatim',
+	},
+    {
+		image: require('../../assets/Sobre/Partner-18.png'),
+		altText: 'Mafc & Filho & RARE & AJP',
+	},
+];
