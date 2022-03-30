252e0218f603368ff6eda71fffdfdec1f53d155a
Sérgio Félix <sergio98@outlook.pt>
2022-03-30 20:35:08 +0100

    Data: services, works, partners for Homepage

---
+++ src/pages/Homepage/_data.tsx
@@ -0,0 +1,99 @@
+export const services = [
+	{
+		size: 'max-w-xs',
+		image: require('../../assets/Homepage/eCommerce.png'),
+		altText: 'Comércio Digital',
+		title: 'Comércio Digital',
+		subTitle: 'Pretende vender on-line?',
+		text: 'Implementamos soluções eCommerce B2C, eCommerce B2B, Order Management System, Headless Commerce e Live eCommerce Stream.',
+		urlTitle: 'Saber mais',
+		internalHref: 'solucoes/comercio-digital',
+	},
+	{
+		size: 'max-w-xs',
+		image: require('../../assets/Homepage/MarketingPerformance.png'),
+		altText: 'Marketing Digital e Performance',
+		title: 'Marketing Digital e Performance',
+		subTitle: 'Procura impulsionar o seu negócio?',
+		text: 'Somos focados em Estratégia Digital, Design e Gestão de Redes Sociais, Performance e E-mail Marketing, Consultoria e Advising Digital.',
+		urlTitle: 'Saber mais',
+		internalHref: 'solucoes/marketing-digital-performance',
+	},
+	{
+		size: 'max-w-xs',
+		image: require('../../assets/Homepage/UIUxDesign.png'),
+		altText: 'UI/UX Design',
+		title: 'UI/UX Design',
+		subTitle: 'Pretende proporcionar uma melhor experiência?',
+		text: 'Somos focados em melhorar a experiência de produtos e serviços digitais, trazendo resultados incríveis para o negócio.',
+		urlTitle: 'Saber mais',
+		internalHref: 'solucoes/uiux-design',
+	},
+	{
+		size: 'max-w-xs',
+		image: require('../../assets/Homepage/Software,CRM,Data.png'),
+		altText: 'Web Development',
+		title: 'Web Development',
+		subTitle: 'Faz tarefas manualmente?',
+		text: 'Otimize processos, acelere a transformação digital e desenhe novas experiências, tirand melhor partido da tecnologia, obtendo dados em tempo real.',
+		urlTitle: 'Saber mais',
+		internalHref: 'solucoes/web-development',
+	},
+];
+
+export const works = [
+	{
+		size: 'max-w-md',
+		image: require('../../assets/Homepage/eCommerce-Mahrla.png'),
+		altText: 'eCommerce B2C Mahrla',
+		title: 'eCommerce B2C Mahrla',
+		text: 'Implementação de um eCommerce B2C e sistema de gestão de encomendas (OMS), suportados pelas tecnologias Shopify, Node.JS, React.JS e Moloni.',
+		urlTitle: 'Contacte-nos para saber mais',
+		externalHref: 'mailto:askus@basicamente.io?subject=Quero%20saber%20mais%20sobre%20o%20projeto%20Mahrla',
+	},
+	{
+		size: 'max-w-md',
+		image: require('../../assets/Homepage/eCommerceB2Jovaty.png'),
+		altText: 'eCommerce B2B Jovaty',
+		title: 'eCommerce B2B Jovaty',
+		text: 'Implementação de um eCommerce B2B sincronizado com o ERP Primavera, disponibilizando listas de preço por cliente e plataforma de encomendas BSP Commerce.',
+		urlTitle: 'Contacte-nos para saber mais',
+		externalHref: 'mailto:askus@basicamente.io?subject=Quero%20saber%20mais%20sobre%20o%20projeto%20Mahrla',
+	},
+	{
+		size: 'max-w-md',
+		image: require('../../assets/Homepage/Case-Parissete.png'),
+		altText: 'Data Collector Paris:sete',
+		title: 'Data Collector Paris:sete',
+		text: "Implementação de uma solução de Data Collector (web scraping) baseado em Inteligência Artificial, para análise de preço e produtos dos competitors em 300.000 sku's.",
+		urlTitle: 'Contacte-nos para saber mais',
+		externalHref: 'mailto:askus@basicamente.io?subject=Quero%20saber%20mais%20sobre%20o%20projeto%20Mahrla',
+	},
+];
+
+export const partners = [
+	{
+		image: require('../../assets/Homepage/Partner-1.png'),
+		altText: 'Porto. & hôma',
+	},
+	{
+		image: require('../../assets/Homepage/Partner-2.png'),
+		altText: 'mota-engil & Forcargo',
+	},
+	{
+		image: require('../../assets/Homepage/Partner-3.png'),
+		altText: 'P.Porto & Kaizen',
+	},
+	{
+		image: require('../../assets/Homepage/Partner-4.png'),
+		altText: '321 Crédito & Halibut',
+	},
+	{
+		image: require('../../assets/Homepage/Partner-5.png'),
+		altText: 'Mattreness & Clínicas Nuno Mendes',
+	},
+	{
+		image: require('../../assets/Homepage/Partner-6.png'),
+		altText: 'Exaktus & Mahrla',
+	},
+];
