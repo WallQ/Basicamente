5b28029d7096d8a8eb4834d0eaff0646f46ec121
Sérgio Félix <sergio98@outlook.pt>
2022-06-02 20:39:59 +0100

    Refactoring Header

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -6,41 +6,43 @@ import SimpleCard from '../../components/SimpleCard/SimpleCard';
 import InputBox from '../../components/InputBox/InputBox';
 import TextArea from '../../components/TextArea/TextArea';
 
+import Header from '../../components/Header/Header';
+
 // Background Images
-import BannerImage from '../../assets/Homepage/Basicamente-Header.jpeg';
 import FooterImage from '../../assets/Homepage/Basicamente-Footer.jpg';
 
 // Data
 import { services, works, partners } from './_data';
 
+import { useQuery } from '@apollo/client';
+import { HOMEPAGE_CONTENT } from '../../GraphQL/Queries';
+
 const Homepage: React.FunctionComponent = () => {
+	const { error, loading, data } = useQuery(HOMEPAGE_CONTENT);
+
+	console.log(error);
+	console.log(loading);
+	console.log(data);
+
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		alert('Formulário enviado com sucesso!');
 		e.preventDefault();
-	}
+	};
 
 	return (
 		<>
-			<div className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-28 lg:py-44" style={{ backgroundImage: `url(${BannerImage})` }}>
-				<div className="m-auto w-2/3 space-y-8 text-center leading-loose text-white lg:w-1/2">
-					<h1 className="text-xl font-medium lg:text-4xl">
-						Aceleramos a sua Transformação Digital
-					</h1>
-					<p className="whitespace-pre-wrap text-base font-light leading-normal lg:text-2xl">
-						Ajudamos empresas e organizações a{' '}
-						<span className="font-bold">
-							Transformar, Inovar e a Escalar Negócios no Digital
-						</span>
-						, entregando soluções únicas e inovadoras de{' '}
-						<span className="font-bold">Comércio Eletrónico</span>,{' '}
-						<span className="font-bold">UI/UX</span>,{' '}
-						<span className="font-bold">Web Development</span>,
-						<span className="font-bold">Marketing Digital</span> e{' '}
-						<span className="font-bold">Websites</span> e{' '}
-						<span className="font-bold">Workplaces Digitais</span>.
-					</p>
-				</div>
-			</div>
+			{loading ? (
+				<h1>Loading</h1>
+			) : (
+				data.homepageHeaderCollection && (
+					<Header
+						image={data.homepageHeaderCollection.items[0].image.url}
+						title={data.homepageHeaderCollection.items[0].title}
+						text={data.homepageHeaderCollection.items[0].text}
+						button={false}
+					/>
+				)
+			)};
 			<div className="mx-auto max-w-max px-8">
 				<BreakTitle title="Em que podemos ajudar?" />
 				<div className="flex min-w-full flex-col flex-wrap items-center justify-evenly gap-x-8 gap-y-10 align-middle md:flex-row md:items-start">
@@ -76,16 +78,20 @@ const Homepage: React.FunctionComponent = () => {
 				<BreakTitle title="Dezenas de marcas confiam na Basicamente" />
 				<div className="grid grid-cols-1 content-center justify-items-center gap-y-6 pb-12 lg:grid-cols-3">
 					{partners.map((partner, index) => (
-						<img 
-							key={partner.altText + index} 
+						<img
+							key={partner.altText + index}
 							className="w-4/5"
 							src={partner.image}
 							alt={partner.altText}
-							loading="lazy" />
+							loading="lazy"
+						/>
 					))}
 				</div>
 			</div>
-			<div className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-16 lg:py-28" style={{ backgroundImage: `url(${FooterImage})` }}>
+			<div
+				className="flex flex-col items-center justify-center bg-cover bg-center bg-no-repeat py-16 lg:py-28"
+				style={{ backgroundImage: `url(${FooterImage})` }}
+			>
 				<div className="space-y-8 text-center leading-loose text-white lg:w-1/2">
 					<h1 className="text-xl font-medium lg:text-3xl">
 						Basicamente, vamos conversar?
@@ -95,29 +101,61 @@ const Homepage: React.FunctionComponent = () => {
 						disponível a ajudá-lo.
 					</p>
 				</div>
-				<form onSubmit={handleSubmit} className="mt-8 flex w-4/5 flex-col flex-wrap justify-center gap-y-6 text-white lg:w-1/2">
+				<form
+					onSubmit={handleSubmit}
+					className="mt-8 flex w-4/5 flex-col flex-wrap justify-center gap-y-6 text-white lg:w-1/2"
+				>
 					<div className="inline-flex w-full items-end justify-between gap-x-2">
 						<div className="flex grow flex-col">
-							<InputBox id="name" title="Nome" subTitle="Nome *" type="text" />
+							<InputBox
+								id="name"
+								title="Nome"
+								subTitle="Nome *"
+								type="text"
+							/>
 						</div>
 						<div className="flex grow flex-col">
-							<InputBox id="surname" title="Sobrenome" type="text" />
+							<InputBox
+								id="surname"
+								title="Sobrenome"
+								type="text"
+							/>
 						</div>
 					</div>
 					<div className="flex flex-col">
-						<InputBox id="email" title="O seu e-mail *" type="email" />
+						<InputBox
+							id="email"
+							title="O seu e-mail *"
+							type="email"
+						/>
 					</div>
 					<div className="flex flex-col">
-						<InputBox id="telephone" title="Telefone *" type="text" />
+						<InputBox
+							id="telephone"
+							title="Telefone *"
+							type="text"
+						/>
 					</div>
 					<div className="flex flex-col">
-						<InputBox id="company" title="A sua empresa *" type="text" />
+						<InputBox
+							id="company"
+							title="A sua empresa *"
+							type="text"
+						/>
 					</div>
 					<div className="flex flex-col">
-						<TextArea id="message" title="Conte-nos o seu projeto *" row={4} col={2} />
+						<TextArea
+							id="message"
+							title="Conte-nos o seu projeto *"
+							row={4}
+							col={2}
+						/>
 					</div>
 					<div className="flex flex-row justify-center">
-						<button type="submit" className="whitespace-nowrap rounded-none border border-transparent bg-primary px-8 py-4 text-sm font-thin text-white shadow-sm hover:opacity-80">
+						<button
+							type="submit"
+							className="whitespace-nowrap rounded-none border border-transparent bg-primary px-8 py-4 text-sm font-thin text-white shadow-sm hover:opacity-80"
+						>
 							Desejo ser contactado
 						</button>
 					</div>
