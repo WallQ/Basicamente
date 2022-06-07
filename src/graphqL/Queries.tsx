4be269a6974e8ce92cb664d608dcfc735f8b760d
Sérgio Félix <sergio98@outlook.pt>
2022-06-07 21:57:57 +0100

    fix path name

---
+++ src/graphqL/Queries.tsx
@@ -0,0 +1,68 @@
+import { gql } from '@apollo/client';
+
+export const HOMEPAGE_CONTENT = gql`
+	query getHomepageContent {
+		homepageHeaderCollection {
+			items {
+				title
+				text {
+					json
+				}
+				image {
+					title
+					description
+					url
+					sys {
+						id
+					}
+				}
+			}
+		}
+		homepageServicesCollection {
+			items {
+				title
+				question
+				text
+				url
+				image {
+					title
+					description
+					url
+					sys {
+						id
+					}
+				}
+			}
+		}
+		homepageProjectsCollection {
+			items {
+				title
+				text
+				url
+				image {
+					title
+					description
+					url
+					sys {
+						id
+					}
+				}
+			}
+		}
+		homepagePartnersCollection {
+			items {
+				title
+				imagesCollection {
+					items {
+						title
+						description
+						url
+						sys {
+							id
+						}
+					}
+				}
+			}
+		}
+	}
+`;
