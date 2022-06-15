8ff27474297b7e762f4a4ab614a27c00cb970e13
Sérgio Félix <sergio98@outlook.pt>
2022-06-15 02:44:12 +0100

    Testing Route, Component, Others...

---
+++ src/components/Testing/ErrorComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface Props {
+}
+
+const ErrorComponent: React.FunctionComponent<Props> = () => {
+	return (
+		<React.Fragment>
+		</React.Fragment>
+	);
+};
+
+export default ErrorComponent;
---
+++ src/pages/Testing/Testing.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import TestingComponent from '../../components/Testing/TestingComponent';
+
+const Testing: React.FunctionComponent = () => {
+	return (
+		<React.Fragment>
+			<TestingComponent />
+		</React.Fragment>
+	);
+};
+
+export default Testing;
