58c271410a819258bbbd980ed1544c18d1b4f9d9
Sérgio Félix <sergio98@outlook.pt>
2022-06-15 02:45:43 +0100

    Uncomment StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -31,11 +31,11 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-	// <React.StrictMode>
+	<React.StrictMode>
 		<ApolloProvider client={client}>
 			<App />
 		</ApolloProvider>
-	// </React.StrictMode>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
