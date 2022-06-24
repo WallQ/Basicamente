1dcf2f2191f6d342e92855e9d55b3f1f2b7ed017
Sérgio Félix <sergio98@outlook.pt>
2022-06-24 21:19:54 +0100

    N/A

--- src/components/Testing/TestingComponent.tsx
+++ src/components/Testing/TestingComponent.tsx
@@ -5,11 +5,8 @@ interface Props {}
 const TestingComponent: React.FunctionComponent<Props> = () => {
 	return (
 		<React.Fragment>
-			<div className="mx-auto flex flex-col items-center justify-center py-48">
-				<svg className="h-32 w-32 animate-spin text-primary" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-					<circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-					<path className="opacity-100" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-				</svg>
+			<div className="mx-auto flex flex-row items-center justify-center py-12">
+				<div className="h-8 w-1/5 animate-pulse bg-blue-300" />
 			</div>
 		</React.Fragment>
 	);
