bea9b194ac4bb38a20cfa6e149d097cf667f3810
Sérgio Félix <sergio98@outlook.pt>
2022-07-01 18:23:10 +0100

    N/A

--- src/components/Testing/TestingComponent.tsx
+++ src/components/Testing/TestingComponent.tsx
@@ -5,9 +5,6 @@ interface Props {}
 const TestingComponent: React.FunctionComponent<Props> = () => {
 	return (
 		<React.Fragment>
-			<div className="mx-auto flex flex-row items-center justify-center py-12">
-				<div className="h-8 w-1/5 animate-pulse bg-blue-300" />
-			</div>
 		</React.Fragment>
 	);
 };
