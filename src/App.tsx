831a658e12a9d3aa0a1ad3bbafb6187fe3ea1fbf
Sérgio Félix <sergio98@outlook.pt>
2022-07-03 21:41:02 +0100

    More readable code (Array.fill to Array.from)

--- src/App.tsx
+++ src/App.tsx
@@ -12,37 +12,40 @@ import Testing from './pages/Testing/Testing';
 
 // Contexts
 import { MultilingualContextProvider } from './contexts/MultilingualContext';
+import { ToastNotificationContextProvider } from './contexts/ToastNotificationContext';
 
 const App: React.FunctionComponent = () => {
 	return (
 		<MultilingualContextProvider>
-			<Router>
-				<Routes>
-					<Route path="/" element={<Layout />}>
-						<Route path="/test" element={<Testing />} />
-						<Route index element={<Homepage />} />
-						<Route path="/solucoes">
-							<Route path="comercio-digital" element={<h1>/solucoes/comercio-digital</h1>} />
-							<Route path="marketing-digital-performance" element={<h1>/solucoes/marketing-digital-performance</h1>} />
-							<Route path="uiux-design" element={<h1>/solucoes/uiux-design</h1>} />
-							<Route path="websites-workplaces-digitais" element={<h1>/solucoes/websites-workplaces-digitais</h1>} />
-							<Route path="web-development" element={<h1>/solucoes/web-development</h1>} />
+			<ToastNotificationContextProvider>
+				<Router>
+					<Routes>
+						<Route path="/" element={<Layout />}>
+							<Route path="/test" element={<Testing />} />
+							<Route index element={<Homepage />} />
+							<Route path="/solucoes">
+								<Route path="comercio-digital" element={<h1>/solucoes/comercio-digital</h1>} />
+								<Route path="marketing-digital-performance" element={<h1>/solucoes/marketing-digital-performance</h1>} />
+								<Route path="uiux-design" element={<h1>/solucoes/uiux-design</h1>} />
+								<Route path="websites-workplaces-digitais" element={<h1>/solucoes/websites-workplaces-digitais</h1>} />
+								<Route path="web-development" element={<h1>/solucoes/web-development</h1>} />
+							</Route>
+							<Route path="/work">
+								<Route path="websites" element={<h1>/work/websites</h1>} />
+								<Route path="e-commerce" element={<h1>/work/e-commerce</h1>} />
+								<Route path="web-development" element={<h1>/work/web-development</h1>} />
+								<Route path="marketing-digital-performance" element={<h1>/work/marketing-digital-performance</h1>} />
+							</Route>
+							<Route path="sobre-a-basicamente" element={<Sobre />} />
+							<Route path="contactos" element={<Contactos />} />
+							<Route path="pedir-proposta" element={<h1>/pedir-proposta</h1>} />
+							<Route path="termos-e-condicoes" element={<h1>/termos-e-condicoes</h1>} />
+							<Route path="resolucao-litigios" element={<h1>/resolucao-litigios</h1>} />
+							<Route path="*" element={<h1>Error 404!</h1>} />
 						</Route>
-						<Route path="/work">
-							<Route path="websites" element={<h1>/work/websites</h1>} />
-							<Route path="e-commerce" element={<h1>/work/e-commerce</h1>} />
-							<Route path="web-development" element={<h1>/work/web-development</h1>} />
-							<Route path="marketing-digital-performance" element={<h1>/work/marketing-digital-performance</h1>} />
-						</Route>
-						<Route path="sobre-a-basicamente" element={<Sobre />} />
-						<Route path="contactos" element={<Contactos />} />
-						<Route path="pedir-proposta" element={<h1>/pedir-proposta</h1>} />
-						<Route path="termos-e-condicoes" element={<h1>/termos-e-condicoes</h1>} />
-						<Route path="resolucao-litigios" element={<h1>/resolucao-litigios</h1>} />
-						<Route path="*" element={<h1>Error 404!</h1>} />
-					</Route>
-				</Routes>
-			</Router>
+					</Routes>
+				</Router>
+			</ToastNotificationContextProvider>
 		</MultilingualContextProvider>
 	);
 };
--- src/components/ProjectsCard/ProjectsCardLoadingSkeleton.tsx
+++ src/components/ProjectsCard/ProjectsCardLoadingSkeleton.tsx
@@ -8,18 +8,16 @@ const ProjectsCardLoadingSkeleton: React.FunctionComponent<Props> = ({ quantity 
 	return (
 		<React.Fragment>
 			<div className="grid grid-cols-1 items-start justify-items-center gap-8 md:grid-cols-3 lg:grid-cols-3">
-				{Array(quantity)
-					.fill(0)
-					.map((item, index) => (
-						<React.Fragment key={`ProjectsCard-${index}`}>
-							<div className="flex w-full animate-pulse flex-col gap-y-4 bg-blue-200">
-								<div className="h-60 w-full animate-pulse bg-blue-300" />
-								<div className="h-7 w-40 animate-pulse bg-blue-300" />
-								<div className="h-16 w-full animate-pulse bg-blue-300" />
-								<div className="h-7 w-52 animate-pulse bg-blue-300" />
-							</div>
-						</React.Fragment>
-					))}
+				{Array.from({ length: quantity }, (_, index) => (
+					<React.Fragment key={`ProjectsCard-${index}`}>
+						<div className="flex w-full animate-pulse flex-col gap-y-4 bg-blue-200">
+							<div className="h-60 w-full animate-pulse bg-blue-300" />
+							<div className="h-7 w-40 animate-pulse bg-blue-300" />
+							<div className="h-16 w-full animate-pulse bg-blue-300" />
+							<div className="h-7 w-52 animate-pulse bg-blue-300" />
+						</div>
+					</React.Fragment>
+				))}
 			</div>
 		</React.Fragment>
 	);
