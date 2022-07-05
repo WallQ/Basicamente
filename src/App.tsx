d1b74db6dfa1074f4d803f2a7c910c22145960fa
Sérgio Félix <sergio98@outlook.pt>
2022-07-05 18:27:02 +0100

    Some routes fixs

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Layout from './layouts/Main/Main';
 
 // Pages
 import Homepage from './pages/Homepage/Homepage';
-import Sobre from './pages/Sobre/Sobre';
-import Contactos from './pages/Contactos/Contactos';
+import About from './pages/About/About';
+import Contact from './pages/Contact/Contact';
 import Testing from './pages/Testing/Testing';
 
 // Contexts
@@ -23,24 +23,24 @@ const App: React.FunctionComponent = () => {
 						<Route path="/" element={<Layout />}>
 							<Route path="/test" element={<Testing />} />
 							<Route index element={<Homepage />} />
-							<Route path="/solucoes">
-								<Route path="comercio-digital" element={<h1>/solucoes/comercio-digital</h1>} />
-								<Route path="marketing-digital-performance" element={<h1>/solucoes/marketing-digital-performance</h1>} />
-								<Route path="uiux-design" element={<h1>/solucoes/uiux-design</h1>} />
-								<Route path="websites-workplaces-digitais" element={<h1>/solucoes/websites-workplaces-digitais</h1>} />
-								<Route path="web-development" element={<h1>/solucoes/web-development</h1>} />
+							<Route path="/services">
+								<Route path="digital-commerce" element={<h1>/services/digital-commerce</h1>} />
+								<Route path="marketing-digital-performance" element={<h1>/services/marketing-digital-performance</h1>} />
+								<Route path="uiux-design" element={<h1>/services/uiux-design</h1>} />
+								<Route path="websites-workplaces-digitals" element={<h1>/services/websites-workplaces-digitals</h1>} />
+								<Route path="web-development" element={<h1>/services/web-development</h1>} />
 							</Route>
-							<Route path="/work">
-								<Route path="websites" element={<h1>/work/websites</h1>} />
-								<Route path="e-commerce" element={<h1>/work/e-commerce</h1>} />
-								<Route path="web-development" element={<h1>/work/web-development</h1>} />
-								<Route path="marketing-digital-performance" element={<h1>/work/marketing-digital-performance</h1>} />
+							<Route path="/portfolio">
+								<Route path="websites" element={<h1>/portfolio/websites</h1>} />
+								<Route path="e-commerce" element={<h1>/portfolio/e-commerce</h1>} />
+								<Route path="web-development" element={<h1>/portfolio/web-development</h1>} />
+								<Route path="marketing-digital-performance" element={<h1>/portfolio/marketing-digital-performance</h1>} />
 							</Route>
-							<Route path="sobre-a-basicamente" element={<Sobre />} />
-							<Route path="contactos" element={<Contactos />} />
-							<Route path="pedir-proposta" element={<h1>/pedir-proposta</h1>} />
-							<Route path="termos-e-condicoes" element={<h1>/termos-e-condicoes</h1>} />
-							<Route path="resolucao-litigios" element={<h1>/resolucao-litigios</h1>} />
+							<Route path="about" element={<About />} />
+							<Route path="contact" element={<Contact />} />
+							<Route path="get-quote" element={<h1>/get-quote</h1>} />
+							<Route path="terms-conditions" element={<h1>/terms-conditions</h1>} />
+							<Route path="dispute-resolution" element={<h1>/dispute-resolution</h1>} />
 							<Route path="*" element={<h1>Error 404!</h1>} />
 						</Route>
 					</Routes>
