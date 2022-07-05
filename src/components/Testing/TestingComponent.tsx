3049cbe72f6cab613ef6dab1de690cdcb7b9ae27
Sérgio Félix <sergio98@outlook.pt>
2022-07-05 18:28:41 +0100

    N/A

--- src/components/Testing/TestingComponent.tsx
+++ src/components/Testing/TestingComponent.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
-import ToastNotification from '../ToastNotification/ToastNotification';
-import { ToastNotificationContextType, ToastNotificationContext } from '../../contexts/ToastNotificationContext';
 
 interface Props {}
 
 const TestingComponent: React.FunctionComponent<Props> = () => {
-	const { setShow, setSuccess, setMessage } = React.useContext(ToastNotificationContext) as ToastNotificationContextType;
-
-	const handleClick = () => {
-		setSuccess(true);
-		setMessage("We've sent you an email with more details.");
-		setShow(true);
-		setSuccess(false);
-		setMessage("Please try again later.");
-		setShow(true);
-	};
 
 	return (
 		<React.Fragment>
-			<button onClick={handleClick}>CLICK ME!</button>
-			<ToastNotification />
 		</React.Fragment>
 	);
 };
--- src/pages/Testing/Testing.tsx
+++ src/pages/Testing/Testing.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 import TestingComponent from '../../components/Testing/TestingComponent';
 
-import { ToastNotificationContextProvider } from '../../contexts/ToastNotificationContext';
-
 const Testing: React.FunctionComponent = () => {
 	return (
 		<React.Fragment>
-			<ToastNotificationContextProvider>
-				<TestingComponent />
-			</ToastNotificationContextProvider>
+			<TestingComponent />
 		</React.Fragment>
 	);
 };
