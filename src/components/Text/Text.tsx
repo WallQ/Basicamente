b590190310feced37f51592169fc18d85195f007
Sérgio Félix <sergio98@outlook.pt>
2022-07-06 17:30:59 +0100

    Created FC Text

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { DocumentNode, useQuery } from '@apollo/client';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { MultilingualContextType, MultilingualContext } from '../../contexts/MultilingualContext';
+import richTextOptions from '../../utils/richTextOptions';
+
+import LoadingSkeleton from './TextLoadingSkeleton';
+import ErrorBoundary from './TextErrorBoundary';
+
+interface Props {
+	query: DocumentNode;
+}
+
+const Text: React.FunctionComponent<Props> = ({ query }) => {
+	const { language } = React.useContext(MultilingualContext) as MultilingualContextType;
+	const { loading, error, data } = useQuery<any>(query, { variables: { language } });
+
+	if (loading) return <LoadingSkeleton />;
+	if (error) return <ErrorBoundary message={error.message} />;
+
+	return (
+		<React.Fragment>
+			{data && data.text && (
+				<div className="mx-auto max-w-5xl py-48">
+					<div className="flex flex-col flex-wrap items-center justify-center gap-y-4">
+						<div className="whitespace-pre-wrap text-center text-sm font-light text-gray-900">
+                            {documentToReactComponents(data.text.text.json, richTextOptions)}
+						</div>
+					</div>
+				</div>
+			)}
+		</React.Fragment>
+	);
+};
+
+export default Text;
