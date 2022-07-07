96637bb7c48ee31dc557b937bf61ad310381d190
Sérgio Félix <sergio98@outlook.pt>
2022-07-07 17:25:47 +0100

    CSS FIX

--- src/components/Text/TextLoadingSkeleton.tsx
+++ src/components/Text/TextLoadingSkeleton.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const TitleLoadingSkeleton: React.FunctionComponent = () => {
 	return (
 		<React.Fragment>
-			<div className='mx-auto flex flex-row items-center justify-center py-12'>
-				<div className='h-8 w-1/5 animate-pulse bg-blue-300' />
+			<div className="mx-auto flex flex-row items-center justify-center py-12">
+				<div className="flex flex-1 flex-col flex-wrap items-center justify-center gap-y-4">
+					<div className="h-8 w-full animate-pulse bg-blue-300" />
+					<div className="h-8 w-3/4 animate-pulse bg-blue-300" />
+					<div className="h-8 w-3/5 animate-pulse bg-blue-300" />
+					<div className="h-8 w-full animate-pulse bg-blue-300" />
+					<div className="h-8 w-3/4 animate-pulse bg-blue-300" />
+					<div className="h-8 w-full animate-pulse bg-blue-300" />
+					<div className="h-8 w-4/5 animate-pulse bg-blue-300" />
+					<div className="h-8 w-2/5 animate-pulse bg-blue-300" />
+				</div>
 			</div>
 		</React.Fragment>
 	);
