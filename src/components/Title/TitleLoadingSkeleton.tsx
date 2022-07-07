25ae544db6c98ba1f6dcf78f0541e45765dad6d5
Sérgio Félix <sergio98@outlook.pt>
2022-07-07 17:25:53 +0100

    CSS FIX

--- src/components/Title/TitleLoadingSkeleton.tsx
+++ src/components/Title/TitleLoadingSkeleton.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TitleLoadingSkeleton: React.FunctionComponent = () => {
 	return (
 		<React.Fragment>
-			<div className="mx-auto flex flex-row items-center justify-center py-12">
+			<div className="mx-auto flex flex-row items-center justify-center">
 				<div className="h-8 w-1/5 animate-pulse bg-blue-300" />
 			</div>
 		</React.Fragment>
