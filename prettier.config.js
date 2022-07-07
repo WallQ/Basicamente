2271fe1da47029bb558f7b590a21b0fb98c1696a
Sérgio Félix <sergio98@outlook.pt>
2022-07-07 02:13:03 +0100

    Linter and Formatter Config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	bracketSameLine: true,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	jsxSingleQuote: false,
+	printWidth: 80,
+	semi: true,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "all",
+	useTabs: true,
+};
